86437e4233a0088d4ee5d6022b71a7c02011b673
schmudde <schmudde@beyondthefra.me>
2016-06-20 13:31:08 -0400

    audio step sequencer working

--- src/rhythm_of_time/sequencer.cljs
+++ src/rhythm_of_time/sequencer.cljs
@@ -5,12 +5,14 @@
 
 
 (defn setup []
-  (q/frame-rate 15)
+  (q/frame-rate 5)
   (q/color-mode :hsb)
   {:stages '(1 0 0 0 0 0 0 0)
    :frequencies [500 600 700 800 900 1000 1100 1200]
    :stages2 '(0 0 0 1 0 0 0 0)
    :frequencies2 [1000 1200 1400 1600 1800 2000 2200 2400]
+   :audio1 true
+   :audio2 true
    :tempo1 120 ;; currently, nothing > 120 works as a maximum tempo
    :tempo2 60}  ;; currently, 0 does not work as a minimum tempo
   )
@@ -19,7 +21,7 @@
 (defn frame-rate-scaler [tempo]
   "baseline-tempo / frame-rate = tempo / x. If tempo=120 then returns the equivalent to (q/frame-rate)"
   (let [baseline-tempo 120
-        current-frame-rate 15 ;(q/current-frame-rate)
+        current-frame-rate 5 ;(q/current-frame-rate)
         scaled-tempo (.round js/Math (/ (* tempo current-frame-rate) baseline-tempo))]
     (+ (- current-frame-rate scaled-tempo ) 1)))
 
@@ -33,9 +35,11 @@
   "advances the sequencer to the next stage and the next frequency"
   (if (advance? tempo)
     {:stage (drop-last (conj stages (last stages)))
-     :frequency (subvec (conj frequency-list (first frequency-list)) 1)}
+     :frequency (subvec (conj frequency-list (first frequency-list)) 1)
+     :audio true}
     {:stage stages
-     :frequency frequency-list}))
+     :frequency frequency-list
+     :audio false}))
 
 
 (defn update-state [state]
@@ -48,8 +52,10 @@
         sequencer2 (advancer stages2 frequency-list2 (:tempo2 state))]
       {:stages (:stage sequencer1)
        :frequencies (:frequency sequencer1)
+       :audio1 (:audio sequencer1)
        :stages2 (:stage sequencer2)
        :frequencies2 (:frequency sequencer2)
+       :audio2 (:audio sequencer2)
        :tempo1 120
        :tempo2 60}
 ))
@@ -58,6 +64,11 @@
   "takes a stage state, 0 or 1, and prepares it for drawing"
   (* stage-state 255))
 
+(defn gain-value [audio-on-off]
+  (if audio-on-off
+    0.1
+    0.0))
+
 (defn draw-stage! [stage position-x position-y]
     (q/fill 255 235 95 stage)
     (q/ellipse (+ 100 (* position-x 20)) (+ 220 position-y) 15 15))
@@ -65,10 +76,10 @@
 (defn draw-state [state]
   "Side effect: draws state onto screen"
   (q/background 200)
-;  (synth/ping! (first (:frequencies state)) 0.1)
-;  (synth/ping! (first (:frequencies2 state)) 0.1)
 
   (let [length-of-sequencer (count (:stages state))]
+    (synth/ping! (first (:frequencies state)) (gain-value (:audio1 state)))
+    (synth/ping! (first (:frequencies2 state)) (gain-value (:audio2 state)))
     (dotimes [i length-of-sequencer]
       (draw-stage! (stage-value (nth (:stages state) i)) i 0)
       (draw-stage! (stage-value (nth (:stages2 state) i)) i 20))
--- src/rhythm_of_time/synthesizer.cljs
+++ src/rhythm_of_time/synthesizer.cljs
@@ -3,13 +3,13 @@
 
 (defonce context (bach/audio-context))
 
-(defn ping [freq]
+(defn ping [freq gain]
   (bach/connect->
    (bach/square freq)
    (bach/percussive 0.01 1.0)
-   (bach/gain 0.1)))
+   (bach/gain gain)))
 
-(defn ping! [hertz time]
-  (-> (ping hertz)
+(defn ping! [hertz gain]
+  (-> (ping hertz gain)
       (bach/connect-> bach/destination)
-      (bach/run-with context (bach/current-time context) time)))
+      (bach/run-with context (bach/current-time context) 0.1)))
